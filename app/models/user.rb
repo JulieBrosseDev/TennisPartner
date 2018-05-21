61cc99851229b21f83ef2f2c3c8ed169d659b56b
Mathieu Duprez <duprez.math@gmail.com>
2018-05-21 17:38:15 +0200

    validation modele User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :answers
+  has_many :reciever_answers, foreign_key: "receiver_id"
+
+  validates :name, presence: true
+  validates :ranking, presence: true
+  validates :gender, presence: true
+  validates :age, presence: true
+
 end
+
