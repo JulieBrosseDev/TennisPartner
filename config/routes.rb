cb8781ee0a4a74f5cb132752841ae95991f106b3
Julie Brosse <juliebrosse31@gmail.com>
2018-05-21 17:44:36 +0200

    routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'users#index'
+  resources :answers, only:[:create, :show] do
+    resources :conversations, only: :create
+  end
+  resources :conversations, only: :show do
+    resources :messages, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
