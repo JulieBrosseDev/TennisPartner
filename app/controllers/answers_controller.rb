4fade925fc1f5ca35706430a048170d17bd0b752
Julie Brosse <juliebrosse31@gmail.com>
2018-05-22 14:49:10 +0200

    answers_controller finished

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,23 @@
 class AnswersController < ApplicationController
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      return redirect_to match_show if @answer.match?
+      redirect_to users_path
+    end
+  end
+
+
   def show
     @answer = Answer.find(params[:id])
   end
 
-  def create
-    @answer = Answer.new(params[:answer])
-    @answer.save
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:status, :user_id, :receiver_id)
   end
+
 end
