21389ee9d70d553a718c33c8160362622fb07e56
Julie Brosse <juliebrosse31@gmail.com>
2018-05-22 11:43:56 +0200

    seed_users_new

--- db/seed.rb
+++ db/seed.rb
@@ -11,35 +11,30 @@ users_attributes = [
     ranking:       2,
     gender:       'Male',
     age:           37
-  }
-  {
-    name:         'Lynn',
-    ranking:       5,
-    gender:       'Female',
-    age:           22
-  }
-]
-User.create!(users_attributes)
-
-puts 'Creating users...'
-users_attributes = [
-  {
-    name:         'Charles',
-    ranking:      4,
-    gender:       'Male',
-    age:           33
   },
   {
-    name:         'Jonathan',
-    ranking:       2,
-    gender:       'Male',
-    age:           37
-  }
-  {
     name:         'Lynn',
     ranking:       5,
     gender:       'Female',
     age:           22
+  },
+  {
+    name:         'Marie',
+    ranking:       1,
+    gender:       'Female',
+    age:           40
+  },
+  {
+    name:         'Louis',
+    ranking:       5,
+    gender:       'Male',
+    age:           22
+  },
+  {
+    name:         'Adrien',
+    ranking:       5,
+    gender:       'Male',
+    age:           15
   }
 ]
 User.create!(users_attributes)
