f81d2082b2595f83b17553b344d1c0d415509f52
Julie Brosse <juliebrosse31@gmail.com>
2018-05-23 19:08:30 +0200

    geocoder finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
 
-  def index(search_radius = 10)
-    if params[:search][:address].present?
-      @users.near(params["search"]["address"], search_radius)
-    else
-      alert
+  def index
       @users = User.displayable_for(current_user)
       @user = User.first
       @answer = Answer.new
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  DEFAULTS = {
+    search_radius: 10,
+  }.freeze
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
@@ -13,7 +17,6 @@ class User < ApplicationRecord
   validates :gender, presence: true
   validates :age, presence: true
   validates :address, presence: true
-  validates :search_radius, presence: true
 
   scope :all_except_I, ->(user) { where.not(id: user) }
   scope :by_opponent_ranking, ->(opponent_ranking) { where(ranking: opponent_ranking) }
@@ -22,10 +25,14 @@ class User < ApplicationRecord
   scope :has_answer, ->(user) { joins(:answers).where(answers: { user: user})}
   scope :has_feedback_by, ->(user) {joins(:receiver_answers).merge(Answer.where(user: user))}
   scope :has_no_feedback_by, ->(user) {where.not(id: has_feedback_by(user))}
-  scope :displayable_for, ->(user) { all_except(user).has_no_feedback_by(user)}
+  scope :displayable_for, ->(user) { all_except(user).has_no_feedback_by(user).near(user.address, user.safe_search_radius)}
 
   def already_liked?(user)
     user.receiver_answers.like.exists?(user: self)
+  end
+
+  def safe_search_radius
+    search_radius || DEFAULT[:search_radius]
   end
 
 end
