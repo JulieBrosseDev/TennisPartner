d2a11ea6e996fd03ed9fdfa2de6ea34757a9f565
Mathieu Duprez <duprez.math@gmail.com>
2018-05-24 19:01:08 +0200

    update js, le like swipe et le bouton like fonctionne

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     opponent_ranking: (1..5),
     name: "No Name",
     address: "",
-    gender: "female",
+    gender: "male",
     ranking: 1,
     age: "",
     picture: "../../assets/images/default.jpg"
@@ -37,12 +37,12 @@ class User < ApplicationRecord
   scope :displayable_for, ->(user) {
     all_except_me(user)
       .has_no_feedback_by(user)
-      .opponent_with_ranking(user.opponent_ranking)
       .near(user.address, user.safe_search_radius)
-      .opponent_with_gender(user.opponent_gender)
+      # .opponent_with_ranking(user.opponent_ranking)
+      # .opponent_with_gender(user.opponent_gender)
     }
 
-  
+
   def already_liked?(user)
     user.receiver_answers.like.exists?(user: self)
   end
