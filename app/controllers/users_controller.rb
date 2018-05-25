9879decd886cfd474b30a94d8d15f752fcdf4cac
Martin Solignac <martin.solignac@gmail.com>
2018-05-25 17:22:59 +0200

    back edit and search preferences done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,27 +6,6 @@ class UsersController < ApplicationController
 
    end
 
-   def edit
-    # @user = User.find(params[:id])
-    super
-   end
 
-   def update
-    byebug
-    @user = User.find(params[:id])
-      if @user.update(user_params)
-      flash[:success] = 'Your profile has been updated.'
-      redirect_to edit_user_registration(@user.user_id)
-    else
-      @user.errors.full_messages
-      flash[:error] = @user.errors.full_messages
-      render :edit
-   end
- end
 
-   private
-
-   def user_params
-    params.require(:user).permit(:name, :ranking, :gender, :age, :address, :picture)
-   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'users#index'
   resources :criterias
   resources :answers, only:[:create, :show] do
