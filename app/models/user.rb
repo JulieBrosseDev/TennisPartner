0c90a5783e810c09bfa3d5aa69d22bfd59459f0a
Martin Solignac <martin.solignac@gmail.com>
2018-05-25 13:02:00 +0200

    resolve conflict with cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,9 +40,9 @@ class User < ApplicationRecord
   scope :displayable_for, ->(user) {
     all_except_me(user)
       .has_no_feedback_by(user)
-      .opponent_with_ranking(user.opponent_ranking)
       .near(user.address, user.safe_search_radius)
-      .opponent_with_gender(user.opponent_gender)
+      # .opponent_with_ranking(user.opponent_ranking)
+      # .opponent_with_gender(user.opponent_gender)
     }
 
 
