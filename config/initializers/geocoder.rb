517231ee1e2ddb9d4224576d7bd1cb38ace467a3
Mathieu Duprez <duprez.math@gmail.com>
2018-05-27 19:33:14 +0200

    Footer et autocomplete

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,7 +3,7 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
    lookup: :google,            # name of geocoding service (symbol)
   # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
+   language: :en,              # ISO-639 language code
    use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
