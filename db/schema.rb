489748105f56a46659963030017d32dcfcaa61b5
Julie Brosse <juliebrosse31@gmail.com>
2018-05-28 10:49:24 +0200

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523152756) do
+ActiveRecord::Schema.define(version: 20180526065726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,25 @@ ActiveRecord::Schema.define(version: 20180523152756) do
     t.datetime "updated_at", null: false
     t.index ["receiver_id"], name: "index_answers_on_receiver_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "receiver_id"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_conversations_on_receiver_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,7 +68,6 @@ ActiveRecord::Schema.define(version: 20180523152756) do
     t.integer "search_radius"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo"
     t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -57,4 +75,6 @@ ActiveRecord::Schema.define(version: 20180523152756) do
 
   add_foreign_key "answers", "users"
   add_foreign_key "answers", "users", column: "receiver_id"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
