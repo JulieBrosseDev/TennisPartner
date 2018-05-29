01fae88101c7c74e975007bddb8b4f9fa2947be9
Martin Solignac <martin.solignac@gmail.com>
2018-05-29 11:30:11 +0200

    facebook connect

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180528094122) do
+ActiveRecord::Schema.define(version: 20180529075410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,9 +66,17 @@ ActiveRecord::Schema.define(version: 20180528094122) do
     t.integer "search_radius"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.string "picture"
     t.string "hand"
     t.string "club"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
