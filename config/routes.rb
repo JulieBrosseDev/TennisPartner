6d9f9b37c3053d1055741579a32c21f1a29ed0b5
Mathieu Duprez <duprez.math@gmail.com>
2018-05-30 16:21:04 +0200

    page profil

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     resources :messages, only: :create
   end
   delete '/logout' => 'registrations#destroy'
+  get '/profile' => 'pages#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
 
