57e528ced797ec94a8a76423bcfe891f6baded14
Julie Brosse <juliebrosse31@gmail.com>
2018-05-30 17:28:41 +0200

    index-cards design

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,10 +1,15 @@
 class ConversationsController < ApplicationController
+
   def create
     @answer = Answer.find(params[:answer_id])
     @conversation = Conversation.new()
     @conversation.users << @answer.user << @answer.receiver
     @conversation.save
     redirect_to conversation_path(@conversation)
+  end
+
+  def index
+    @conversations = current_user.conversations
   end
 
   def show
